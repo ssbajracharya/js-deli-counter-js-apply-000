b26052f76e686287c5a7d4d3d50e8f32e9d44125
Saumya Bajracharya <ssbajracharya@hotmail.com>
2019-02-22 20:56:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function takeANumber(line, name){
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
 
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
 
+  return `Currently serving ${line.shift()}.`
+};
 
 function currentLine(line){
   if(line.length === 0){
